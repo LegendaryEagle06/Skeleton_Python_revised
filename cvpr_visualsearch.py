01779106609e3e66284827aec8e8ed01eb268fec
Sagar <sdesai0296@gmail.com>
2024-10-28 11:56:56 +0000

    TASK 1-7

--- cvpr_visualsearch.py
+++ cvpr_visualsearch.py
@@ -4,7 +4,9 @@ import scipy.io as sio
 import cv2
 from random import randint
 from cvpr_compare import cvpr_compare
-import ipdb
+from sklearn.metrics import precision_recall_curve
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 DESCRIPTOR_FOLDER = 'descriptors'
 DESCRIPTOR_SUBFOLDER = 'globalRGBhisto'
@@ -16,14 +18,17 @@ ALLFILES = []
 for filename in os.listdir(os.path.join(DESCRIPTOR_FOLDER, DESCRIPTOR_SUBFOLDER)):
     if filename.endswith('.mat'):
         img_path = os.path.join(DESCRIPTOR_FOLDER, DESCRIPTOR_SUBFOLDER, filename)
-        img_actual_path = os.path.join(IMAGE_FOLDER,'Images',filename).replace(".mat",".bmp")
-        # ipdb.set_trace()
+        img_actual_path = os.path.join(IMAGE_FOLDER, 'Images', filename).replace(".mat", ".bmp")
         img_data = sio.loadmat(img_path)
         ALLFILES.append(img_actual_path)
         ALLFEAT.append(img_data['F'][0])  # Assuming F is a 1D array
 
 # Convert ALLFEAT to a numpy array
 ALLFEAT = np.array(ALLFEAT)
+
+# Apply PCA for dimensionality reduction
+pca = PCA(n_components=15)
+ALLFEAT = pca.fit_transform(ALLFEAT)
 
 # Pick a random image as the query
 NIMG = ALLFEAT.shape[0]
@@ -49,3 +54,15 @@ for i in range(SHOW):
     cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Precision-Recall Evaluation
+relevant_images = [1 if i < 10 else 0 for i in range(NIMG)]  # Assuming top 10 are relevant
+predicted_scores = [1 / (x[0] + 1e-5) for x in dst]  # Inverse of distance as relevance score
+
+precision, recall, _ = precision_recall_curve(relevant_images, predicted_scores)
+
+# Plot Precision-Recall curve
+plt.plot(recall, precision, marker='.')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.show()
--- extractRandom.py
+++ extractRandom.py
@@ -1,10 +1,14 @@
-
 import numpy as np
+import cv2
 
-def extractRandom(img):
-    # Generate a random row vector with 30 elements
-    F = np.random.rand(1, 30)
-    # Returns a row [rand rand .... rand] representing an image descriptor
-    # computed from image 'img'
-    # Note img is expected to be a normalized RGB image (colors range [0,1] not [0,255])
-    return F
+def extractColorHistogram(img, bins_per_channel=8):
+    # Compute histogram for each channel (R, G, B)
+    hist_r = cv2.calcHist([img], [0], None, [bins_per_channel], [0, 1])
+    hist_g = cv2.calcHist([img], [1], None, [bins_per_channel], [0, 1])
+    hist_b = cv2.calcHist([img], [2], None, [bins_per_channel], [0, 1])
+    
+    # Concatenate and normalize the histograms
+    hist = np.concatenate([hist_r, hist_g, hist_b]).flatten()
+    hist /= hist.sum()  # Normalize to sum to 1
+    
+    return hist
